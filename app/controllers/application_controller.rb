fbe03c9856554dcecac2982445e905614c249ac4
Nikita-Boyarskikh <N02@yandex.ru>
2018-01-02 14:06:20 +0300

    Add localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
   semantic_breadcrumb :index, :root_path
+
+  def set_locale
+    if params[:locale]
+      session[:locale] = params[:locale]
+    end
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
