2be3ecfb30a0ce5cd9bf498b67716db09048f8c1
Nikita-Boyarskikh <N02@yandex.ru>
2018-01-09 21:53:30 +0300

    Add localization

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+# For deploying with Device to Heroku
+# Reference: https://github.com/plataformatec/devise#heroku
+Rails.application.config.assets.initialize_on_precompile = false
